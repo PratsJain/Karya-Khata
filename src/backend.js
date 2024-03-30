2e33f3754bc21fea5af4c70c3c62b21d50129b50
Pratyush Jain <pratyush20396@iiitd.ac.in>
2024-03-30 07:30:08 +0530

    added todo and project factories

---
+++ src/backend.js
@@ -0,0 +1,135 @@
+const { v4: uuidv4 } = require('uuid');
+
+function getPriCol(pr) {
+    const mapping = ["red", "orange", "yellow", "blue", "aqua"];
+    return mapping[pr - 1];
+}
+
+
+function toDo(Title, Desc, DueDate, Priority, Notes, Status = false) {
+    const id = uuidv4();
+    let title = Title, desc = Desc, dueDate = DueDate, priority = Priority, notes = Notes, isDone = Status;
+    const setTitle = (tl) => { title = tl };
+    const getTitle = () => title;
+    const setDesc = (descp) => { desc = descp };
+    const getDesc = () => desc;
+    const setdueDate = (date) => { dueDate = date };
+    const getdueDate = () => dueDate;
+    const setPrior = (prior) => { priority };
+    const getPrior = () => priority;
+    const setNotes = (note) => { notes = note };
+    const getNotes = () => notes;
+    const setStatus = (status) => { isDone = status };
+    const getStatus = () => isDone;
+    const getId = () => id;
+    const getPriorCol = () => {
+        return getPriCol(priority);
+    };
+    const getData = () => {
+        return { id, title, desc, dueDate, priority, notes, isDone };
+    };
+    const stringify = () => {
+        return JSON.stringify(getData());
+    };
+
+    return { setDesc, setNotes, setPrior, setStatus, setTitle, setdueDate, getdueDate, getDesc, getId, getNotes, getStatus, getTitle, getPrior, getPriorCol, stringify };
+}
+
+function project(Title) {
+    let title = Title;
+    const id = uuidv4();
+    const toDos = {};
+    const getId = () => id;
+    const getTitle = () => title;
+    const setTitle = (heading) => { title = heading };
+    const addToDo = (todo_item) => {
+        toDos[todo_item.getId()] = todo_item;
+    };
+    const removeToDo = (todoId) => {
+        if (toDos.hasOwnProperty(todoId)) {
+            delete toDos[todoId];
+        }
+    };
+    const updateTodo = (todoId, heading, desc, notes, duedate, prior, status) => {
+        if (toDos.hasOwnProperty(todoId)) {
+            toDos[todoId].setDesc(desc);
+            toDos[todoId].setNotes(notes);
+            toDos[todoId].setTitle(heading);
+            toDos[todoId].setdueDate(duedate);
+            toDos[todoId].setPrior(prior);
+            toDos[todoId].setStatus(status);
+        }
+    };
+    const toggleStatus = (todoId, status) => {
+        if (toDos.hasOwnProperty(todoId)) {
+            toDos[todoId].setStatus(status);
+        };
+    }
+    const getTodo = (todoId) => {
+        if (toDos.hasOwnProperty(todoId)) {
+            const { getdueDate, getDesc, getId, getNotes, getStatus, getTitle, getPrior, getPriorCol, stringify } = toDos[todoId];
+            return { getdueDate, getDesc, getId, getNotes, getStatus, getTitle, getPrior, getPriorCol, stringify };
+        }
+    };
+
+    const getTodoAll = () => {
+        const res = [];
+        for (let key in toDos) {
+            if (toDos.hasOwnProperty(key)) {
+                let { getdueDate, getDesc, getId, getNotes, getStatus, getTitle, getPrior, getPriorCol, stringify } = toDos[key];
+                res.push({ getdueDate, getDesc, getId, getNotes, getStatus, getTitle, getPrior, getPriorCol, stringify });
+            }
+        }
+        return res;
+    };
+
+    const getData = () => {
+        const toDos2 = [];
+        for (let key in toDos) {
+            if (toDos.hasOwnProperty(key)) {
+                toDos2.push(toDos[key].stringify());
+            }
+        }
+        return { id, title, toDos2 };
+    };
+    const stringify = () => {
+        return JSON.stringify(getData());
+    };
+
+    return { getTitle, getId, getTodo, getTodoAll, stringify, updateTodo, removeToDo, addToDo, toggleStatus, setTitle };
+}
+
+
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        const x = "__storage_test__";
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return (
+            e instanceof DOMException &&
+            // everything except Firefox
+            (e.code === 22 ||
+                // Firefox
+                e.code === 1014 ||
+                // test name field too, because code might not be present
+                // everything except Firefox
+                e.name === "QuotaExceededError" ||
+                // Firefox
+                e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage &&
+            storage.length !== 0
+        );
+    }
+}
+
+
+
+function backendUser() {
+
+}
+
