7a4d7bde9ba0ec1499e8555cbc4429a8c615451e
Pratyush Jain <pratyush20396@iiitd.ac.in>
2024-04-06 05:22:06 +0530

    sidebar initial commit

--- src/frontend.js
+++ src/frontend.js
@@ -1,17 +1,53 @@
 export default function frontend() {
+
+    document.querySelector(".backdrop").addEventListener("click", () => {
+        document.querySelector('.backdrop').classList.toggle("show");
+        document.querySelector('.popup-modal').classList.toggle("show");
+    });
+
     const fav_icon = (img) => {
         const favicon = document.createElement('link');
 
         favicon.rel = 'icon';
         favicon.type = 'image/png';
-        favicon.href = img; 
+        favicon.href = img;
         const existingFavicon = document.querySelector('link[rel="icon"]');
         if (existingFavicon) {
             document.head.removeChild(existingFavicon);
         }
         document.head.appendChild(favicon);
-    }
+    };
 
-    
-    return {fav_icon};
+    const loginForm = () => {
+        document.querySelector('.backdrop').classList.toggle("show");
+        document.querySelector('.popup-modal').classList.toggle("show");
+    };
+    const updateName = (uname) => {
+        try {
+            document.querySelector(".user-name").textContent = uname;
+            const ncard = document.querySelector(".name-card");
+            // ncard.classList.remove("sidebar-anim");
+            ncard.classList.add("sidebar-anim");
+        }
+        catch (error) {
+            console.log("No such element");
+        }
+    };
+    const renderSidebar = (user, pic) => {
+        const uname = user.getName();
+        const ncard = document.createElement("div");
+        ncard.classList.add("name-card");
+        ncard.classList.add("sidebar-anim");
+        const propic = new Image();
+        propic.src = pic;
+        propic.classList.add("user-image");
+        const nameDiv = document.createElement("p");
+        nameDiv.classList.add("user-name");
+        nameDiv.textContent = uname;
+        ncard.appendChild(propic);
+        ncard.appendChild(nameDiv);
+        ncard.addEventListener('click', loginForm);
+        document.querySelector(".sidebar").appendChild(ncard);
+    };
+    return { fav_icon, loginForm, renderSidebar, updateName };
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ const app = (function ScreenController() {
     const front = frontend();
     front.fav_icon(fav);
     const user = User();
-
+    if (!user.getisData()) {
+        front.loginForm();
+    }
+    document.querySelector(".login-form-container").addEventListener("submit", (event) => {
+        event.preventDefault();
+        const fname = document.querySelector("#fname").value;
+        const lname = document.querySelector("#lname").value;
+        const userName = fname + " " + lname;
+        user.setName(userName);
+        document.querySelector('.backdrop').classList.toggle("show");
+        document.querySelector('.popup-modal').classList.toggle("show");
+        front.updateName(userName);
+    });
+    front.renderSidebar(user, man);
 })();
 
